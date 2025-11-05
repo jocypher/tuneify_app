281df132434746901d8fc853685f0b5a6fded1af
willyjo1 <arthurwilchield@gmail.com>
2025-11-05 11:00:46 +0000

    feat: initial stage of the tuneify app

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:tuneify_app/nav_bar.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tuneify',
+      debugShowCheckedModeBanner: false,
+      
+      home: const NavBar(),
+    );
+  }
+}
